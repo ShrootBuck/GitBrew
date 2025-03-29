f5558255622f95b2eeb95b44d1a31a6724d3a4e6
ShrootBuck <ShrootBuck@users.noreply.github.com>
2025-03-28 21:13:42 -0700

    f

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import { auth } from "~/server/auth";
 import Landing from "./onboarding/0";
 import GithubApp from "./onboarding/1";
+import { db } from "~/server/db";
 
 export default async function HomePage() {
   const session = await auth();
 
   if (session) {
+    // Get the user
+    const user = await db.user.findUnique({ where: { id: session.user.id } });
+
     return <GithubApp />;
   }
   return <Landing />;
