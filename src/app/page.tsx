557a1609d658a56af7f5c64b265e83da89c6d871
ShrootBuck <ShrootBuck@users.noreply.github.com>
2025-04-01 15:05:12 -0700

    forgot I changed this to home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,7 +25,7 @@ export default async function HomePage() {
       case 3:
         return <AddressForm />;
       case 4:
-        return redirect("/dashboard");
+        return redirect("/home");
     }
   }
 
