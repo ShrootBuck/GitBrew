805d1e7ae4c339dbc2ce2393709c4667f1ccd167
ShrootBuck <ShrootBuck@users.noreply.github.com>
2025-03-31 19:37:26 -0700

    AQWREFSGRdzhtgfrdef

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,10 @@ import Landing from "./onboarding/0";
 import GithubApp from "./onboarding/1";
 import { db } from "~/server/db";
 import TerminalConnect from "./onboarding/2";
+import { redirect } from "next/navigation";
+import Terminal from "@terminaldotshop/sdk";
+import { getValidTerminalToken } from "~/server/terminalUtils";
+import { env } from "~/env";
 
 export default async function HomePage() {
   const session = await auth();
@@ -17,7 +21,14 @@ export default async function HomePage() {
       case 1:
         return <TerminalConnect />;
       case 2:
-        return <p>Hello!! Terminal Authed; now to add a credit card.</p>;
+        const accessToken = await getValidTerminalToken(user.id);
+
+        const terminal = new Terminal({
+          bearerToken: accessToken,
+          baseURL: env.TERMINAL_API_URL,
+        });
+        const card = await terminal.card.collect();
+        return redirect(card.data.url);
     }
   }
   return <Landing />;
